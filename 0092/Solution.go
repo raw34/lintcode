2a792c9c56b0f4c7918a31a611c22f64173ed6a7
raw34 <raw34@sina.com>
2022-01-07 17:54:27 +0800

    update

--- 0092/Solution.go
+++ 0092/Solution.go
@@ -1,10 +1,15 @@
 package _0092
 
-/**
- * @param m: An integer m denotes the size of a backpack
- * @param A: Given n items with size A[i]
- * @return: The maximum size
- */
+/*
+m = 10, A = [3, 4, 8, 5]
+dp = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3]
+    [0, 0, 0, 3, 4, 4, 4, 7, 7, 7, 7]
+    [0, 0, 0, 3, 4, 4, 4, 7, 8, 8, 8]
+    [0, 0, 0, 3, 4, 5, 5, 7, 8, 9, 9]
+]
+*/
 func backPack(m int, A []int) int {
     max := func(a, b int) int {
         if a < b {
@@ -13,6 +18,7 @@ func backPack(m int, A []int) int {
         return a
     }
     n := len(A)
+    // dp[i][j]表示取前i个元素且背包容量为j时能达到的最大值
     dp := make([][]int, n+1)
     for i := 0; i < n+1; i++ {
         dp[i] = make([]int, m+1)
@@ -31,6 +37,10 @@ func backPack(m int, A []int) int {
     return dp[n][m]
 }
 
+/*
+m = 10, A = [3, 4, 8, 5]
+dp = [0 0 0 3 4 5 5 7 8 9 9]
+*/
 func backPack2(m int, A []int) int {
     max := func(a, b int) int {
         if a < b {
@@ -39,6 +49,7 @@ func backPack2(m int, A []int) int {
         return a
     }
     n := len(A)
+    // dp[j]表示取前i个元素且背包容量为j时能达到的最大值
     dp := make([]int, m+1)
     for i := 1; i < n+1; i++ {
         a := A[i-1]
