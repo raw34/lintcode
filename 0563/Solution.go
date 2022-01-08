ca904e7ef3ed9661b8065ac947f874735f990125
raw34 <raw34@sina.com>
2022-01-08 08:57:11 +0800

    update

--- 0563/Solution.go
+++ 0563/Solution.go
@@ -22,9 +22,8 @@ func backPackV(nums []int, target int) int {
     for i := 1; i < n+1; i++ {
         w := nums[i-1]
         for j := 0; j < target+1; j++ {
-            if j < w {
-                dp[i][j] = dp[i-1][j]
-            } else {
+            dp[i][j] = dp[i-1][j]
+            if j >= w {
                 dp[i][j] = dp[i-1][j] + dp[i-1][j-w]
             }
         }
